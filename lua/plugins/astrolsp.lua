ed620c68508b81202469e116407e2e6b49184c4d
Zachery Faria <zacheryfaria@gmail.com>
2025-07-31 10:52:08 -0700

    stop using eslint

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -30,6 +30,9 @@ return {
         -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
         -- "lua_ls",
         -- "vtsls",
+        "eslint_d",
+        "eslint",
+        --"vtsls",
       },
       timeout_ms = 1000, -- default format timeout
       -- filter = function(client) -- fully override the default formatting function
